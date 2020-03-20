3737865480055fd2eaaabd83da2681fc3c333219
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2020-03-20 08:42:26 -0700

    update

--- linode/build_base.sh
+++ linode/build_base.sh
@@ -6,71 +6,79 @@
 ## bash build_base.sh <hostname> |& tee /tmp/build.log
 ##
 
-# take first argument, or master's hostname as hostname
-host="${1:-`hostname`}"
-private_ip="$2"
+set -eu
+
+error_exit () { echo "Error: $1"; exit 1; }
+
+[ "$#" -eq 2 ] || error_exit "Usage: $0 <hostname> <private_ip>"
+
+host=$1
+private_ip=$2
 
 DEV=/dev/sda
 DIST=testing
 PKGS=udev,locales,ifupdown,systemd-sysv,netbase,net-tools,cron,logrotate,procps,openssh-server,ntp
 
-rootfs=/mnt
-mirror=http://fremont.mirrors.linode.com/debian
+ROOTFS=/mnt
+MIRROR=http://fremont.mirrors.linode.com/debian
+NIC_NAME=enp0s3
+
+[ -b "$DEV" ] || error_exit "$DEV does not exist."
 
 ## keyring
-echo "deb $mirror testing main" > /etc/apt/sources.list
-apt update
+echo "deb $MIRROR testing main" > /etc/apt/sources.list
+apt update || echo 'Ignore error from apt update.'
 apt install -y --force-yes debian-archive-keyring debootstrap
 
 
-mount $DEV $rootfs
+mount $DEV $ROOTFS || error_exit "Failed mount $DEV $ROOTFS."
 
-debootstrap --variant=minbase --arch=amd64 --include=$PKGS $DIST $rootfs $mirror
+debootstrap --variant=minbase --arch=amd64 --include=$PKGS $DIST $ROOTFS $MIRROR || error_exit 'Failed install Debian minbase.'
 
-echo $host > $rootfs/etc/hostname
+echo $host > $ROOTFS/etc/hostname
 
 ## locale
-echo 'en_US.UTF-8 UTF-8' >> $rootfs/etc/locale.gen
-chroot $rootfs locale-gen en_US.UTF-8 UTF-8
-chroot $rootfs update-locale LANG=en_US.UTF-8
+echo 'en_US.UTF-8 UTF-8' >> $ROOTFS/etc/locale.gen
+chroot $ROOTFS locale-gen en_US.UTF-8 UTF-8
+chroot $ROOTFS update-locale LANG=en_US.UTF-8
 
 ## timezone
 cat /etc/timezone
-# unlink $rootfs/etc/localtime
-echo 'America/Los_Angeles' > $rootfs/etc/timezone
-chroot $rootfs dpkg-reconfigure -f noninteractive tzdata
+# unlink $ROOTFS/etc/localtime
+echo 'America/Los_Angeles' > $ROOTFS/etc/timezone
+chroot $ROOTFS dpkg-reconfigure -f noninteractive tzdata
 
 ## fstab
-cat <<EOF > $rootfs/etc/fstab
+cat <<EOF > $ROOTFS/etc/fstab
 /dev/sda    /       ext4    noatime,errors=remount-ro               0 1
 tmpfs       /tmp    tmpfs   rw,nodev,nosuid,noexec,noatime,size=30% 0 0
 tmpfs       /run    tmpfs   rw,nodev,nosuid,noexec,noatime,size=6%  0 0
 EOF
 
 ## network
-cat $rootfs/etc/network/interfaces
+cat $ROOTFS/etc/network/interfaces
 
 gateway=$(/sbin/ip route | awk '/default/ { print $3 }')
 nic=$(/sbin/ip route | awk '/default/ { print $5 }')
 ip=$(/sbin/ip route | awk '!/default/ && /'$nic'/ { print $NF, $1 }' | head -n 1 | sed 's! .*/!/!')
 
-cat <<EOF > $rootfs/etc/network/interfaces
+cat <<EOF > $ROOTFS/etc/network/interfaces
 auto lo
 iface lo inet loopback
 
-auto $nic
-allow-hotplug $nic
-iface $nic inet static
+auto $NIC_NAME
+allow-hotplug $NIC_NAME
+iface $NIC_NAME inet static
     address $ip
     gateway $gateway
-#    up   ip addr add $private_ip/17 dev $nic label $nic:1
-#    down ip addr del $private_ip/17 dev $nic label $nic:1
+    up   ip addr add $private_ip/17 dev $NIC_NAME label $NIC_NAME:1
+    down ip addr del $private_ip/17 dev $NIC_NAME label $NIC_NAME:1
 EOF
 
 ## root password
 password="$(dd if=/dev/urandom bs=16 count=1 2>/dev/null | base64)"
-echo "root:$password" | chroot $rootfs chpasswd
+echo "root:$password" | chroot $ROOTFS chpasswd
 echo "Root password is '$password', please change !"
 echo "Please also verify NIC IP addresses"
 
-umount $rootfs
+umount $ROOTFS  || error_exit "Failed umount $ROOTFS."
